47b1d27a9430cd3774d15c22c28d509e3a2713f3
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-02-16 23:50:18 -0600

    Initial Commit

---
+++ Reporter/Reporter.h
@@ -0,0 +1,19 @@
+//
+//  Reporter.h
+//  Reporter
+//
+//  Created by Jeffrey Fulton on 2017-02-16.
+//  Copyright © 2017 Jeffrey Fulton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reporter.
+FOUNDATION_EXPORT double ReporterVersionNumber;
+
+//! Project version string for Reporter.
+FOUNDATION_EXPORT const unsigned char ReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reporter/PublicHeader.h>
+
+
